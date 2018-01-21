e9726f3fc402fb580e9513600714e5ddca1b7c72
fiddlesticks971 <mitchaheim@gmail.com>
2018-01-21 16:15:40 -0600

    add opencl detection, platform detection and device detection

--- dots.py
+++ dots.py
@@ -1,16 +1,43 @@
+import pyopencl
 
+def openclDetection():
+    print("OpenCl Detection:")
+    print(pyopencl.VERSION)
+    if pyopencl.VERSION == "":
+        return -1
+    else:
+        return 0
 
 def deviceDetection():
     print("Detecting Devices")
+    
+    platformList = pyopencl.get_platforms()
+    if len(platformList) == 0:
+        return -1
+    print ("Number of Platforms: " + str(len(platformList)))
+    
+    deviceList = platformList[0].get_devices()
+    if len(deviceList) == 0:
+        return -1
+    print ("Number of Devices: " + str(len(deviceList)))
+    
+    return 0
+    
 def deviceInitialization():
     print("Device Initialization")
+    
 def applicationInitialization():
     print("Application Initialization")
+    
 def updateDisplay():
     print("Update Display")
 
-def main():    
-    deviceDetection()
+def main():
+    OpenCLError = 0
+    if openclDetection() == -1:
+        return
+    if deviceDetection() == -1:
+        return
     deviceInitialization()
     applicationInitialization()
     i = 6
